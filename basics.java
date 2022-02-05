5efdbe1efec8564b636315d82a4eb78216368320
Prasannah Venkatesh U <88711909+PrasannahVenkatesh@users.noreply.github.com>
2022-02-05 19:34:05 +0530

    Add files via upload

---
+++ basics.java
@@ -0,0 +1,43 @@
+/* CLASS: 
+ * 		A class is a user defined blueprint or prototype from which objects are created. 
+ * 
+ * OBJECT: 
+ * 		An object is created from/using class. Its basic unit of OOPS and may number of object can be 
+ * created.
+ * 
+ * METHOD: 
+ * 		A method is a collection of statements that perform some specific task and return the result to the caller. 
+ * A method can perform some specific task without returning anything. 
+ * Methods allow us to reuse the code without retyping the code. 
+ * 
+ * SIGNATURE:
+ * 		It consists of the method name and a parameter list 
+ * (number of parameters, type of the parameters and order of the parameters).
+ * 
+ */
+public class basics {
+
+	public static void main(String[] args) {
+		// By using System.out.print() we can print.
+		System.out.println("My Name: Prasannah Venkatesh U"); 
+		
+		// - Single line Comment
+		
+		/* 
+		 * Multi line Commment
+		 * System.out.println("Multi Line Comment")
+		 */ 
+		
+		/** 
+		 * Documentation Comment
+		 * 
+		 * System.out.println("Documentation Comment")
+		 * 
+		 * @author Prasannah
+		 * @version 1.0 
+		 * @since 2000-03-14
+		 */
+
+	}
+
+}
