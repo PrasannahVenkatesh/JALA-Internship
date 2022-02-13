b8e447d6a9ef1f36006329084fbacb8e11ecb085
Prasannah Venkatesh U <88711909+PrasannahVenkatesh@users.noreply.github.com>
2022-02-13 17:13:58 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,16 @@
+
+public class B  extends A{
+	
+	public void examName() {
+		System.out.println("NEET Exam");
+	}
+	
+	public void course() {
+		System.out.println("MBBS");
+	}
+	
+	public void field() {
+		System.out.println("Doctor");
+	}
+
+}
---
+++ C.java
@@ -0,0 +1,16 @@
+
+public class C  extends B{
+	
+	public void examName() {
+		System.out.println("SSC Exam");
+	}
+	
+	public void post() {
+		System.out.println("All Over India");
+	}
+	
+	public void elgibility() {
+		System.out.println("Graduate");
+	}
+
+}
