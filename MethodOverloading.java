fd4c8876f08ad916aee1cb900ceb3370ccc46cfb
Prasannah Venkatesh U <88711909+PrasannahVenkatesh@users.noreply.github.com>
2022-02-18 10:19:00 +0000

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,26 @@
+package MethodOverloading;
+
+// Same name, Same number of parameters, same data types methods will give error.
+public class MethodOverloading {
+	
+	public void multiply(int x) {
+		System.out.println("Same Base and Power: "+ (x*x));
+	}
+	public void multiply(int x,int y) {
+		System.out.println("Base : "+x+" Power : "+y+" = "+(int)(Math.pow(x, y)));
+	}
+	
+	public void multiply(double x,double y) {
+		System.out.println("Base : "+x+" Power : "+y+" = "+(Math.pow(x, y)));
+	}
+	
+	public void add(int x,int y) {
+		System.out.println("Addition: "+ (x+y));
+	}
+	
+	public void add(double x, double y) {
+		System.out.println("Addition: "+ (x+y));
+	}
+	
+
+}
